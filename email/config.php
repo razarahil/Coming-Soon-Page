c3b3cbe29438c1d1b074519c9d4f5f3563792a13
Raza Rahil Hussain <razarahil@gmail.com>
2016-03-07 12:43:16 +0530

    First Commit

---
+++ email/config.php
@@ -0,0 +1,8 @@
+<?php
+$hostname="localhost";
+$database="comingsoon";
+$username="raza";
+$password="Razas#pass1";
+
+$sql=new PDO("mysql;host=".$hostnae.";dbname=".$database,$username,$password);
+?>
